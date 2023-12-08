e3f49adb76c0cae417fbfc654889c015c23ada49
isixe <isixe@outlook.com>
2023-12-09 01:46:11 +0800

    test(spider): Update spider test example

--- test/spider_test.py
+++ test/spider_test.py
@@ -4,16 +4,17 @@
 # @Author  : Exisi
 # @Version : python3.10.6
 # @Desc    : base spider test
-
-from spider import jobspider51
+import sqlite3
+import pandas as pd
 from spider.area import areaspider51
+from spider import jobspider51, logger
 
 
 def area():
     areaspider51.start(save_engine='both')
 
 
-def job():
+def part_spider():
     param = {
         "keyword": "Python",
         "page": 1,
@@ -23,6 +24,54 @@ def job():
     jobspider51.start(args=param, save_engine='both')
 
 
+def full_spider(save_engine: str):
+    save_to = {
+        'csv': lambda x: full_spider_csv(x),
+        'db': lambda x: full_spider_db(x),
+        'both': lambda x: full_spider_csv(x)
+    }
+    save = save_to[save_engine]
+    save(save_engine)
+
+
+def full_spider_csv(type: str):
+    df = pd.read_csv('../output/area/51area.csv', header=None, names=None, skiprows=1, delimiter=',')
+
+    for area in df[0]:
+        param = {
+            "keyword": "Python",
+            "page": 1,
+            "pageSize": 1000,
+            "area": area
+        }
+        jobspider51.start(args=param, save_engine=type)
+
+
+def full_spider_db(type: str):
+    results = None
+    connect = sqlite3.connect("../output/area/51area.db")
+    cursor = connect.cursor()
+    sql = '''SELECT `code` FROM `area51`;'''
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        connect.commit()
+    except Exception as e:
+        logger.warning("SQL execution failure of SQLite: " + str(e))
+    finally:
+        cursor.close()
+        connect.close()
+
+    for area in results:
+        param = {
+            "keyword": "Python",
+            "page": 1,
+            "pageSize": 1000,
+            "area": area[0]
+        }
+        jobspider51.start(args=param, save_engine=type)
+
+
 if __name__ == '__main__':
     area()
-    job()
+    full_spider(save_engine='both')
